d437b8df942477deee87290d18a973d519527cdc
german <gdegiovanni@leren.com.ar>
2024-04-09 05:22:45 -0300

    test 12

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,7 +11,7 @@ const store = useAuthStore()
 </script>
 
 <template>
-  <div class="bg-gray-200 min-h-screen flex items-center justify-center">
+  <div class="bg-gray-200 min-h-screen flex items-center justify-center a">
     <div class="bg-gray-700 p-8 rounded-lg shadow-lg max-w-md">
       <h2 class="text-2xl font-bold mb-4 text-white">Login</h2>
       <form @submit.prevent="store.login(username, password)">
